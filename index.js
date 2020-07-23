4d7a7cdd45079703a3ae8d71614e68a451b23a81
nelson-singleton <nsingleton@ymail.com>
2020-07-23 18:40:49 -0500

    MVP Tasks complete. No stretch goals finished

--- index.js
+++ index.js
@@ -156,8 +156,8 @@ class Instructor extends Lambdasian{
   demo(subject){
     return `Today we are learning about ${subject}`;
   }
-  grade({student}, subject){
-    return `${student} receives a perfect score on ${subject}`;
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
   }
 }
 
@@ -210,10 +210,23 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-  constructor()
+class ProjectManager extends Instructor{
+  constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+    super({name, age, location, specialty, favLanguage, catchPhrase});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+    }
+    standUp(channel){
+      return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject){
+      return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+    
 
-}
+  }
+
+
 
 /*
   STRETCH PROBLEM (no tests!)
